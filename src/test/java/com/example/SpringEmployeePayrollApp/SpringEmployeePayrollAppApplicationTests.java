e46c035f3c6257a5d9312de14c5521c3e125ba79
Purvansh Sharma <purvanshsharma6182gmail.com>
2025-02-25 12:38:21 +0530

    [Purvansh Sharma]Add. UC1 features added

---
+++ src/test/java/com/example/SpringEmployeePayrollApp/SpringEmployeePayrollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringEmployeePayrollApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmployeePayrollAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
